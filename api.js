516944d0f8b454ce2fd9a03846bc55746424b42d
Lucas Dantas <lucascamposdantas@hotmail.com>
2020-09-23 17:01:17 -0300

    Finalização da api

--- api.js
+++ api.js
@@ -8,24 +8,23 @@ const server = new koa();
 server.use(bodyparser());
 
 
-// Controlador do produtos // 
+// -------------------------------- Controlador dos produtos -------------------------------- // 
 
 const controladorPOSTProdutos = (ctx) => {
 
     const pathArquivosProdutos = ('./produtosApi.json');
-    const file = fs.readFileSync(pathArquivosProdutos, 'utf8');
-
-    const JSONFile = JSON.parse(file);
+    const produtosFile = fs.readFileSync(pathArquivosProdutos, 'utf8');
+    const JSONProdutosFile = JSON.parse(produtosFile.toString());
+    
     const body = ctx.request.body;
 
     console.log(body);
 
-
-    const criacaoDoProduto = funcoesProdutos.criarProduto(body);
+    const criacaoDoProduto = funcoesProdutos.criarProduto(body, JSONProdutosFile);
 
     if (criacaoDoProduto) {
-        JSONFile.push(criacaoDoProduto);
-        fs.writeFileSync(pathArquivosProdutos, JSON.stringify(JSONFile, null, 4), 'utf8');
+        JSONProdutosFile.push(criacaoDoProduto);
+        fs.writeFileSync(pathArquivosProdutos, JSON.stringify(JSONProdutosFile, null, 4), 'utf8');
         ctx.status = 200;
         ctx.body =     {
             status: 'sucesso',
@@ -35,10 +34,207 @@ const controladorPOSTProdutos = (ctx) => {
         ctx.status = 500;
         ctx.body = 'Não foi possível criar produto';
     }
+};
+
+const controladorGETProdutos = (ctx) => {
+
+    const id = ctx.url.split("/")[2];
+    const pathArquivosProdutos = ('./produtosApi.json');
+    const produtosFile = fs.readFileSync(pathArquivosProdutos, 'utf8');
+    const JSONProdutosFile = JSON.parse(produtosFile.toString());
+
+    console.log(id);
+
+    if (id != "") {
+        const idObtido = funcoesProdutos.pegarProduto(id, JSONProdutosFile);
+
+        if (idObtido) {
+            ctx.status = 200
+            ctx.body = {
+                status: 'sucesso',
+                dados: idObtido,
+            }
+        } else {
+            ctx.status = 500
+            ctx.body = 'Produto não encontrado'
+        }
+    } else {
+        ctx.body = funcoesProdutos.listarProdutos(JSONProdutosFile);
+    }
+};
+
+const controladorDELETEProdutos = (ctx) => {
+
+    const id = ctx.url.split("/")[2];
+    const pathArquivosProdutos = ('./produtosApi.json');
+    const produtosFile = fs.readFileSync(pathArquivosProdutos, 'utf8');
+    const JSONProdutosFile = JSON.parse(produtosFile.toString());
+
+    console.log(id);
+
+    if (id) {
+        const idObtido = funcoesProdutos.deletarProduto(id, JSONProdutosFile);
+        if (idObtido === false) {
+            ctx.status = 500;
+            ctx.body = 'Não foi possível encontrar o ID do produto desejado!';
+        } else {
+            ctx.body = {
+                status: 'sucesso',
+                dados: idObtido,
+            }
+            fs.writeFileSync(pathArquivosProdutos, JSON.stringify(JSONProdutosFile, null, 4), 'utf8');
+        }
+    } else {
+        // aa
+    ctx.status = 400;
+    ctx.body = "É necessário um id para atualizar";
+}};
+
+const controladorPUTProdutos = (ctx) => {
+    const id = ctx.url.split("/")[2];
+    const pathArquivosProdutos = ('./produtosApi.json');
+    const produtosFile = fs.readFileSync(pathArquivosProdutos, 'utf8');
+    const JSONProdutosFile = JSON.parse(produtosFile.toString());
+
+    console.log(id);
+
+    if (id != '') {
+        const body = ctx.request.body;
+        const atributoAModificar = body.atributoAModificar;
+        const modificacao = body.modificacao
+        const idObtido = funcoesProdutos.atualizarProduto(id, atributoAModificar, modificacao, JSONProdutosFile)
+        console.log(idObtido)
+        
+        if(idObtido != undefined) {
+            ctx.body = {
+                status: 'sucesso',
+                dados: idObtido,
+            }
+            fs.writeFileSync(pathArquivosProdutos, JSON.stringify(JSONProdutosFile, null, 4), 'utf8');
+        } else {
+            ctx.status = 400;
+            ctx.body = "Bad Request";
+        }
+    } else {
+        ctx.status = 400;
+        ctx.body = "É necessário um id para atualizar";
+    }
 }
 
+// -------------------------------- Controlador dos pedidos -------------------------------- //
 
+const controladorPOSTPedidos = (ctx) => {
 
+    const pathArquivosProdutos = ('./produtosApi.json');
+    const pathArquivosPedidos = ('./pedidosApi.json');
+    const produtosFile = fs.readFileSync(pathArquivosProdutos, 'utf8');
+    const pedidosFile = fs.readFileSync(pathArquivosPedidos, 'utf8');
+
+    const JSONProdutosFile = JSON.parse(produtosFile.toString());
+    const JSONPedidosFile = JSON.parse(pedidosFile.toString());
+    const body = ctx.request.body;
+
+    const criacaoDoPedido = funcoesPedidos.criarPedido(body, JSONPedidosFile, JSONProdutosFile);
+    if (criacaoDoPedido != null) {
+        JSONPedidosFile.push(criacaoDoPedido)
+        fs.writeFileSync(pathArquivosPedidos, JSON.stringify(JSONPedidosFile, null, 4), 'utf8');
+        fs.writeFileSync(pathArquivosProdutos, JSON.stringify(JSONProdutosFile, null, 4), 'utf8');
+        ctx.status = 200;
+        ctx.body = {
+            status: 'sucesso',
+            dados: body,
+        }
+    } else {
+        ctx.status = 500;
+        ctx.body = 'Não foi possível fazer o pedido.';
+    }
+};
+
+const controladorGETPedidos = (ctx) => {
+
+    const id = ctx.url.split("/")[2];
+    const pathArquivosPedidos = ('./pedidosApi.json');
+    const pedidosFile = fs.readFileSync(pathArquivosPedidos);
+    const JSONPedidosFile = JSON.parse(pedidosFile.toString());
+
+    console.log(id);
+
+    if (id != "") {
+        const idObtido = funcoesPedidos.pegarPedido(id, JSONPedidosFile);
+
+        if (idObtido) {
+            ctx.body = {
+                status: 'sucesso',
+                dados: idObtido,
+            }
+        } else {
+            ctx.status = 500
+            ctx.body = 'Pedido não encontrado.'
+        }
+    } else {
+        ctx.body = funcoesPedidos.listarPedidos(JSONPedidosFile);
+    }
+};
+
+const controladorDELETEPedidos = (ctx) => {
+
+    const id = ctx.url.split("/")[2];
+    const pathArquivosPedidos = ('./pedidosApi.json');
+    const pedidosFile = fs.readFileSync(pathArquivosPedidos, 'utf8');
+    const JSONPedidosFile = JSON.parse(pedidosFile.toString());
+
+    console.log(id);
+
+    if (id) {
+        const idObtido = funcoesPedidos.deletarPedido(id, JSONPedidosFile);
+        if (idObtido === false) {
+            ctx.status = 500;
+            ctx.body = 'Não foi possível encontrar o ID do pedido desejado!'
+        } else {
+            ctx.body = {
+                status: 'sucesso',
+                dados: idObtido,
+            }
+            fs.writeFileSync(pathArquivosPedidos, JSON.stringify(JSONPedidosFile, null, 4), 'utf8');
+        }
+    }
+};
+
+const controladorPUTPedidos = (ctx) => {
+
+    const id = ctx.url.split("/")[2];
+    const pathArquivosPedidos = ('./pedidosApi.json');
+    const pathArquivosProdutos = ("./produtosApi.json");
+    const pedidosFile = fs.readFileSync(pathArquivosPedidos, 'utf8');
+    const produtosFile = fs.readFileSync(pathArquivosProdutos, 'utf8');
+    const JSONPedidosFile = JSON.parse(pedidosFile.toString());
+    const JSONProdutosFile = JSON.parse(produtosFile.toString());
+
+    console.log(id);
+
+    if (id != '') {
+        const body = ctx.request.body;
+        const modificacao = body.modificacao
+        const idProduto = body.idProduto
+        const quantidade = body.quantidade
+        const idObtidoPedidos = funcoesPedidos.modificarEstadoDoPedido(id, modificacao, JSONPedidosFile);
+        funcoesPedidos.atualizarQtdDosPedidos(idObtidoPedidos.idPedido, idProduto, quantidade, JSONPedidosFile, JSONProdutosFile);
+
+        if (idObtidoPedidos) {
+            ctx.body = {
+                status: 'sucesso',
+                dados: idObtidoPedidos
+            }
+            fs.writeFileSync(pathArquivosPedidos, JSON.stringify(JSONPedidosFile, null, 4), 'utf8')
+        } else {
+            ctx.status = 400;
+            ctx.body = "Bad Request"
+        }
+    } else {
+        ctx.status = 400;
+        ctx.body = "É necessário um id para atualizar";
+    }
+}
 
 server.use((ctx) => {
 
@@ -82,7 +278,7 @@ server.use((ctx) => {
         if (method === 'POST') {
             controladorPOSTPedidos(ctx);
         } else if (method === 'GET') {
-                controladorGETPedidos(ctx);
+            controladorGETPedidos(ctx);
         } else if (method === 'DELETE') {
             controladorDELETEPedidos(ctx);
         } else if (method === 'PUT') {
@@ -91,4 +287,4 @@ server.use((ctx) => {
     }
 });
 
-server.listen(8081, () => console.log('O servidor está rodando na porta 8081!'));
+server.listen(8081, () => console.log('O servidor está rodando na porta 8081!'))
